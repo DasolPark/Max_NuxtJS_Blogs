3059c6c2f66fc77119e95f7eeddfae8eb87724fa
Dasol Park <dasol.park615@gmail.com>
2020-09-30 20:24:08 +0900

    33. Working on the Post Page

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      id="1"
+      thumbnail="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmmsolutions.biz%2Fwp-content%2Fgallery%2Fhi-tech%2FHi%2520-%2520Tech%25203.jpg&f=1&nofb=1"
+      title="Hello there!"
+      previewText="This my first post!"
+    />
+    <PostPreview
+      id="2"
+      thumbnail="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmmsolutions.biz%2Fwp-content%2Fgallery%2Fhi-tech%2FHi%2520-%2520Tech%25203.jpg&f=1&nofb=1"
+      title="hi there!"
+      previewText="This my second post!"
+    />
+    <PostPreview
+      id="3"
+      thumbnail="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmmsolutions.biz%2Fwp-content%2Fgallery%2Fhi-tech%2FHi%2520-%2520Tech%25203.jpg&f=1&nofb=1"
+      title="bye there!"
+      previewText="This my third post!"
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview';
+
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
+
+<style>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,35 +3,16 @@
     <section class="intro">
       <h1>Get the latest tech news!</h1>
     </section>
-    <section class="featured-posts">
-      <PostPreview
-        id="1"
-        thumbnail="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmmsolutions.biz%2Fwp-content%2Fgallery%2Fhi-tech%2FHi%2520-%2520Tech%25203.jpg&f=1&nofb=1"
-        title="Hello there!"
-        previewText="This my first post!"
-      />
-      <PostPreview
-        id="2"
-        thumbnail="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmmsolutions.biz%2Fwp-content%2Fgallery%2Fhi-tech%2FHi%2520-%2520Tech%25203.jpg&f=1&nofb=1"
-        title="hi there!"
-        previewText="This my second post!"
-      />
-      <PostPreview
-        id="3"
-        thumbnail="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmmsolutions.biz%2Fwp-content%2Fgallery%2Fhi-tech%2FHi%2520-%2520Tech%25203.jpg&f=1&nofb=1"
-        title="bye there!"
-        previewText="This my third post!"
-      />
-    </section>
+    <PostList />
   </div>
 </template>
 
 <script>
-import PostPreview from '@/components/Posts/PostPreview';
+import PostList from '@/components/Posts/PostList';
 
 export default {
   components: {
-    PostPreview
+    PostList
   }
 }
 </script>
