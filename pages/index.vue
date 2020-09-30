98c9d12b6f94a7226eb8d88cad6a2adb114c7271
Dasol Park <dasol.park615@gmail.com>
2020-09-30 19:58:22 +0900

    29. Splitting the Page into Components

--- pages/index.vue
+++ pages/index.vue
@@ -4,31 +4,35 @@
       <h1>Get the latest tech news!</h1>
     </section>
     <section class="featured-posts">
-      <nuxt-link :to="'/posts/' + 1"  class="post-preview">
-        <article>
-          <div class="post-thumbnail" style="background-image: url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmmsolutions.biz%2Fwp-content%2Fgallery%2Fhi-tech%2FHi%2520-%2520Tech%25203.jpg&f=1&nofb=1')"></div>
-          <div class="post-content">
-            <h1>Post Title 1</h1>
-            <p>Preview Text 1</p>
-          </div>
-        </article>
-      </nuxt-link>
-      <nuxt-link :to="'/posts/' + 2"  class="post-preview">
-        <article>
-          <div class="post-thumbnail" style="background-image: url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmmsolutions.biz%2Fwp-content%2Fgallery%2Fhi-tech%2FHi%2520-%2520Tech%25203.jpg&f=1&nofb=1')"></div>
-          <div class="post-content">
-            <h1>Post Title 2</h1>
-            <p>Preview Text 2</p>
-          </div>
-        </article>
-      </nuxt-link>
+      <PostPreview
+        id="1"
+        thumbnail="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmmsolutions.biz%2Fwp-content%2Fgallery%2Fhi-tech%2FHi%2520-%2520Tech%25203.jpg&f=1&nofb=1"
+        title="Hello there!"
+        previewText="This my first post!"
+      />
+      <PostPreview
+        id="2"
+        thumbnail="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmmsolutions.biz%2Fwp-content%2Fgallery%2Fhi-tech%2FHi%2520-%2520Tech%25203.jpg&f=1&nofb=1"
+        title="hi there!"
+        previewText="This my second post!"
+      />
+      <PostPreview
+        id="3"
+        thumbnail="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmmsolutions.biz%2Fwp-content%2Fgallery%2Fhi-tech%2FHi%2520-%2520Tech%25203.jpg&f=1&nofb=1"
+        title="bye there!"
+        previewText="This my third post!"
+      />
     </section>
   </div>
 </template>
 
 <script>
-export default {
+import PostPreview from '@/components/Posts/PostPreview';
 
+export default {
+  components: {
+    PostPreview
+  }
 }
 </script>
 
@@ -70,41 +74,5 @@ export default {
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-}
-
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
 }
 </style>
