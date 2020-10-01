5290a71fe1f466050d5af245a5faac836a0bfe41
Dasol Park <dasol.park615@gmail.com>
2020-10-01 16:16:46 +0900

    Assignment 2: Time to Practice - Server-Side Data

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="posts-page">
-    <PostList />
+    <PostList :posts="loadPosts" />
   </div>
 </template>
 
@@ -10,6 +10,31 @@ import PostList from '@/components/Posts/PostList';
 export default {
   components: {
     PostList
+  },
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadPosts: [
+          {
+            id: '1',
+            title: 'First post!',
+            previewText: 'First preview post!',
+            thumbnail: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmmsolutions.biz%2Fwp-content%2Fgallery%2Fhi-tech%2FHi%2520-%2520Tech%25203.jpg&f=1&nofb=1"
+          },
+          {
+            id: '2',
+            title: 'Second post!',
+            previewText: 'Second preview post!',
+            thumbnail: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmmsolutions.biz%2Fwp-content%2Fgallery%2Fhi-tech%2FHi%2520-%2520Tech%25203.jpg&f=1&nofb=1"
+          }
+        ]
+      })
+    }, 1500);
+  },
+  data() {
+    return {
+      loadPosts: [],
+    }
   }
 }
 </script>
