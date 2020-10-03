9f65b0c48524509520e90e34fe29ca9dea35fdec
Dasol Park <dasol.park615@gmail.com>
2020-10-03 14:22:09 +0900

    56. Fetching Data from the Backend

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import Vuex from 'vuex';
+import axios from 'axios';
 
 const createStore = () => {
   return new Vuex.Store({
@@ -12,37 +13,15 @@ const createStore = () => {
     },
     actions: {
       nuxtServerInit(vuexContext, context) {
-        if (!process.client) {
-          console.log(context.req);
-          console.log(context.req.session);
-        }
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            vuexContext.commit('setPosts', [
-              {
-                id: "1",
-                title: "First post!",
-                previewText: "First preview post!",
-                thumbnail:
-                  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmmsolutions.biz%2Fwp-content%2Fgallery%2Fhi-tech%2FHi%2520-%2520Tech%25203.jpg&f=1&nofb=1",
-              },
-              {
-                id: "2",
-                title: "Second post!",
-                previewText: "Second preview post!",
-                thumbnail:
-                  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmmsolutions.biz%2Fwp-content%2Fgallery%2Fhi-tech%2FHi%2520-%2520Tech%25203.jpg&f=1&nofb=1",
-              },
-            ])
-            resolve();
-          }, 1000);
-        })
-          .then((data) => {
-            context.store.commit("setPosts", data.loadedPosts);
+        return axios.get('https://nuxt-blog-e7bbf.firebaseio.com/posts.json')
+          .then(res => {
+            const postsArray = [];
+            for (const key in res.data) {
+              postsArray.push({ ...res.data[key], id: key });
+            }
+            vuexContext.commit('setPosts', postsArray)
           })
-          .catch((e) => {
-            context.error(e);
-          });
+          .catch(e => context.error(e));
       },
       setPosts(vuexContext, posts) {
         vuexContext.commit('setPosts', posts);
