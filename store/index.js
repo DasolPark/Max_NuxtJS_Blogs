7cfcfdec8e5946dd3e65e9eaa005520bf97c3de5
Dasol Park <dasol.park615@gmail.com>
2020-10-04 18:10:38 +0900

    84. Fixing the Logout Timer

--- store/index.js
+++ store/index.js
@@ -83,17 +83,17 @@ const createStore = () => {
             console.log(result);
             vuexContext.commit('setToken', result.idToken);
             localStorage.setItem('token', result.idToken);
-            localStorage.setItem('tokenExpiration', new Date().getTime() + result.expiresIn * 1000);
+            localStorage.setItem(
+              'tokenExpiration',
+              new Date().getTime() + +result.expiresIn * 1000
+            );
             Cookie.set('jwt', result.idToken);
-            Cookie.set('expirationDate', new Date().getTime() + result.expiresIn * 1000)
-            vuexContext.dispatch('setLogoutTimer', result.expiresIn * 1000);
+            Cookie.set(
+              'expirationDate',
+              new Date().getTime() + Number.parseInt(result.expiresIn) * 1000
+            );
           })
           .catch((e) => console.log(e));
-      },
-      setLogoutTimer(vuexContext, duration) {
-        setTimeout(() => {
-          vuexContext.commit('clearToken')
-        }, duration);
       },
       initAuth(vuexContext, req) {
         let token;
@@ -116,12 +116,13 @@ const createStore = () => {
         } else {
           token = localStorage.getItem('token');
           expirationDate = localStorage.getItem('tokenExpiration');
-          if (new Date().getTime() > +expirationDate || !token) {
-            return;
-          }
         }
-
-        vuexContext.dispatch('setLogoutTimer', +expirationDate - new Date().getTime())
+        // console.log(new Date().getTime(), +expirationDate);
+        if (new Date().getTime() > +expirationDate || !token) {
+          console.log('No token or invalid token');
+          vuexContext.commit('clearToken');
+          return;
+        }
         vuexContext.commit('setToken', token);
       }
     },
