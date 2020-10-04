c09dc806c1a4913513490cc7e61bfd5e328f225f
Dasol Park <dasol.park615@gmail.com>
2020-10-04 17:24:23 +0900

    81. Invalidating the Token

--- store/index.js
+++ store/index.js
@@ -19,6 +19,9 @@ const createStore = () => {
       },
       setToken(state, token) {
         state.token = token;
+      },
+      clearToken(state) {
+        state.token = null;
       }
     },
     actions: {
@@ -78,8 +81,14 @@ const createStore = () => {
           .then((result) => {
             console.log(result);
             vuexContext.commit('setToken', result.idToken);
+            vuexContext.dispatch('setLogoutTimer', result.expiresIn * 1000);
           })
           .catch((e) => console.log(e));
+      },
+      setLogoutTimer(vuexContext, duration) {
+        setTimeout(() => {
+          vuexContext.commit('clearToken')
+        }, duration);
       }
     },
     getters: {
